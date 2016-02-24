6fc32c168fb38581c4eb403f839f6bf8654c3ed2
Michał Wadas <michalwadas@gmail.com>
2016-02-24 16:47:34 +0100

    * Added Whirlpool 3 hash. * Optimized performance by reducing DOM operations. * Added timing and operations per second (but it's flawed due to dependency on browser render loop).

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
-if (location.protocol === "http:" && location.hostname !== 'localhost') location.protocol = 'https';
+if (location.protocol === "http:" && location.hostname !== 'localhost') {
+    location.protocol = 'https';
+}
 const hashes = [];
 const input = document.querySelector('input');
 const inputOnChange = function (e) {
@@ -8,17 +10,20 @@ const inputOnChange = function (e) {
     }
     e.preventDefault();
     const inputValue = input.value;
-    location.hash = 'input='+encodeURIComponent(inputValue);
+    location.hash = 'input=' + encodeURIComponent(inputValue);
+    const startTime = (window.performance || Date).now();
     (function next(i, render, results) {
-        if (i >= hashes.length) render(results);
-        const hashMethod = hashes[i];
-        if (hashMethod === undefined) {
-            console.warn(e, hashes, i);
+        if (i >= hashes.length) {
+            render(results, startTime);
+            return;
         }
+        const hashMethod = hashes[i];
         hashMethod.digest(input.value, function (err, result) {
-            if (err) throw err;
+            if (err) {
+                throw err;
+            }
             results.push({
-                name: hashMethod.name,
+                name:   hashMethod.name,
                 result: result
             });
             next(i + 1, render, results);
@@ -28,24 +33,44 @@ const inputOnChange = function (e) {
 }.bind(input);
 
 hashes.push({
-    name: 'Original text',
-    digest: function (str, done) {
-        Promise.resolve(str).then(function () {
-            done(null, str);
-        })
-    }
-}, {
-    name: 'MD5',
-    digest: function(str,done) {
-        Promise.resolve(md5(str)).then(function (res) {
-            done(null, res);
-        });
-    }
-});
+        name:   'Original text',
+        digest: function (str, done) {
+            Promise.resolve(str).then(function () {
+                done(null, str);
+            }).catch(done);
+        }
+    },
+    {
+        name: 'Bytes',
+        digest: function(str, done) {
+            var cut = false;
+            if (str.length > 40) {
+                cut=true;
+                str=str.slice(0,40);
+            }
+            Promise.resolve(stringToBuffer(str)).then(function(buffer) {
+                var ret = [].map.call(buffer, function(number) {
+                    return ['00', number.toString(16).toUpperCase()].join('').slice(-2);
+                }).join(':');
+                if (cut) {
+                    ret += '...';
+                }
+                done(null,ret);
+            }).catch(done);
+        }
+    },
+    {
+        name:   'MD5',
+        digest: function (str, done) {
+            Promise.resolve(md5(str)).then(function (res) {
+                done(null, res);
+            }).catch(done);
+        }
+    });
 hashes.push.apply(hashes,
     ['SHA-1', 'SHA-256', 'SHA-384', 'SHA-512'].map(function (hashName) {
         return {
-            name: hashName,
+            name:   hashName,
             digest: function (str, done) {
                 crypto.subtle.digest(hashName, stringToBuffer(str)).then(function (buffer) {
                     done(null, hex(buffer));
@@ -56,20 +81,54 @@ hashes.push.apply(hashes,
         };
     })
 );
+hashes.push({
+    name:   'Whirlpool 3.0',
+    digest: function (str, done) {
+        Promise.resolve(Whirlpool(str)).then(function (res) {
+            done(null, res);
+        }).catch(done);
+    }
+});
 
-function render(results) {
-    const tbody = document.querySelector('tbody');
-    tbody.innerHTML = results.map(function (el) {
-        return '<tr><td class="hash-method">' + el.name + '</td><td class="hash-result">' + el.result + '</td>';
-    }).join('\n');
-}
+const render = (function(){
+    const hashCache = {};
+    let firstRun = true;
+    const timing = document.querySelector('#timing');
+    const ops = document.querySelector('#ops-per-sec');
+    return function render(results, startTime) {
+        const endTime = (window.performance || Date).now();
+        timing.textContent = String(Number((endTime-startTime).toFixed(3)));
+        ops.textContent = (1000/(endTime-startTime)).toFixed(1);
+        if (firstRun) {
+            firstRun = false;
+            const tbody = document.querySelector('tbody');
+            results.forEach(function(el) {
+                const tr = document.createElement('tr');
+                const hashMethod = document.createElement('td');
+                hashMethod.textContent = el.name;
+                hashMethod.setAttribute('class', 'hash-method');
+                const hashResult = document.createElement('td');
+                hashResult.setAttribute('class', 'hash-result');
+                hashCache[el.name] = hashResult;
+                hashResult.textContent = el.result;
+                tr.appendChild(hashMethod);
+                tr.appendChild(hashResult);
+                tbody.appendChild(tr);
+            })
+        } else {
+            results.forEach(function(el) {
+                hashCache[el.name].textContent = el.result;
+            });
+        }
+
+
+    };
+}());
 
 
 document.querySelector('#compute').addEventListener('click', inputOnChange);
 input.addEventListener('input', inputOnChange);
 input.addEventListener('onchange', inputOnChange);
-
-
 
 // Taken from here: https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
 // Public domain anyway
@@ -96,13 +155,15 @@ function stringToBuffer(str) {
 }
 
 document.querySelector('#auto').checked = (localStorage.auto || 'true') === 'true';
-document.querySelector('#auto').addEventListener('change', function(){
+document.querySelector('#auto').addEventListener('change', function () {
     localStorage.auto = String(this.checked);
     if (this.checked) {
-        inputOnChange({preventDefault: function(){}})
+        inputOnChange({
+            preventDefault: function () {
+            }
+        })
     }
 });
-
 
 if (location.hash.startsWith('#input=')) {
     document.querySelector('input').value = decodeURIComponent(location.hash.slice('#input='.length))
